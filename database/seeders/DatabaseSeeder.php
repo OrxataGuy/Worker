85724242bae9e22ba45a3a1692f1aa8d38f9d4f2
OrxataGuy <orxataguy@gmail.com>
2023-03-09 09:54:07 +0100

    reg

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->technologies();
+        //$this->technologies();
     }
 
     public function technologies() : void
