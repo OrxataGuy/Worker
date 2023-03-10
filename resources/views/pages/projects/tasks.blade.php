c51a79635bdfead7e4899d3cfbea33226d58f071
OrxataGuy <orxataguy@gmail.com>
2023-03-10 13:22:09 +0100

    tables

--- resources/views/pages/projects/tasks.blade.php
+++ resources/views/pages/projects/tasks.blade.php
@@ -94,7 +94,7 @@
                         </i>
                     </a>
                     <a class="btn btn-secondary btn-sm" href="#" onclick="configTime({{ $task->id }})">
-                        <i class="fas fa-gears"></i>
+                        <i class="fas fa-pencil-alt"></i>
                     </a>
                     @else
                     <a class="btn btn-warning btn-sm" onclick="reopen({{ $task->id }}, 0)" href="#">
