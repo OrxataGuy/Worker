50c4a43ff6e34b12485729253c363c083a364437
OrxataGuy <orxataguy@gmail.com>
2023-03-10 13:21:23 +0100

    tables

--- resources/views/pages/projects/task.blade.php
+++ resources/views/pages/projects/task.blade.php
@@ -75,7 +75,7 @@
                           <span class="showHide">Finalizar</span></i>
                     </a>
                     <a class="btn btn-secondary btn-sm" href="#" onclick="configTime({{ $task->id }})">
-                        <i class="fas fa-gears">
+                        <i class="fas fa-pencil">
                           <span class="showHide">Corregir</span></i>
                     </a>
                 </td>
