7aee1f779bfc4993e02c4df743af451c34845fd3
OrxataGuy <orxataguy@gmail.com>
2023-03-11 13:16:01 +0100

    tech

--- routes/web.php
+++ routes/web.php
@@ -33,8 +33,15 @@ Route::group(['middleware' => ['auth', 'only-dev']], function () {
     Route::post('upload', [Docs::class, 'store'])->name('upload');
 
     Route::resource('clients', Clients::class)->only('index','store','create','show','update','destroy');
-    Route::resource('projects', Projects::class)->only('index','store','show','update','destroy');
     Route::get('projects/create/{client}', [Projects::class, 'create'])->name('projects.create');
+    Route::resource('payments', Payments::class)->only('store','update','destroy');
+    Route::group(['prefix' => 'payments'], function () {
+        Route::get('{client}/view', [Payments::class, 'view'])->name('payments.view');
+    });
+});
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('projects', Projects::class)->only('index','store','show','update','destroy');
     Route::get('projects/{project}/tasks', [Tasks::class, 'index'])->name('tasks.index');
     Route::resource('tasks', Tasks::class)->only('show','store','update');
     Route::group(['prefix' => 'tasks/{task}'], function () {
@@ -49,43 +56,4 @@ Route::group(['middleware' => ['auth', 'only-dev']], function () {
             Route::put('{info}/delete', [Tasks::class, 'delInfo'])->name('tasks.info.del');
         });
     });
-    Route::resource('payments', Payments::class)->only('store','update','destroy');
-    Route::group(['prefix' => 'payments'], function () {
-        Route::get('{client}/view', [Payments::class, 'view'])->name('payments.view');
-    });
-
-
-
-   /* Route::group(['prefix' => 'projects'], function () {
-        Route::get('/', [Projects::class, 'index'])->name('projects');
-        Route::get('get', [Projects::class, 'get'])->name('project.get');
-        Route::get('add/{client}', [Projects::class, 'add'])->name('project.add');
-        Route::post('create', [Projects::class, 'create'])->name('project.create');
-        Route::put('update', [Projects::class, 'update'])->name('project.update');
-        Route::delete('delete', [Projects::class, 'delete'])->name('project.delete');
-        Route::post('payment', [Payments::class, 'create'])->name('project.pay');
-
-        Route::group(['prefix' => 'payments'], function() {
-            Route::get('{id}', [Payments::class, 'view'])->name('payment.view');
-            Route::put('pay', [Payments::class, 'update'])->name('payment.update');
-            Route::delete('delete-payment', [Payments::class, 'delete'])->name('payment.delete');
-        });
-
-        Route::group(['prefix' => '{project}'], function () {
-            Route::get('/', [Tasks::class, 'index'])->name('tasks');
-
-            Route::get('/view/{task}', [Tasks::class, 'view'])->name('task.view');
-            Route::post('toggle', [Tasks::class, 'toggleCounter'])->name('tasks.toggle');
-            Route::post('reopen', [Tasks::class, 'reopen'])->name('tasks.reopen');
-
-            Route::get('get', [Tasks::class, 'get'])->name('tasks.get');
-            Route::post('create', [Tasks::class, 'create'])->name('tasks.create');
-            Route::put('update', [Tasks::class, 'update'])->name('tasks.update');
-            Route::put('finish', [Tasks::class, 'finish'])->name('tasks.finish');
-
-            Route::post('add', [Tasks::class, 'addInfo'])->name('tasks.add.details');
-            Route::put('del', [Tasks::class, 'delInfo'])->name('tasks.del.details');
-
-        });
-    });*/
 });
