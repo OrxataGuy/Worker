0e40532f57c20bbc64e8075f4b494bec54ed17be
OrxataGuy <orxataguy@gmail.com>
2023-03-11 12:53:01 +0100

    tech

--- database/migrations/2023_03_11_114028_create_table_task_time.php
+++ database/migrations/2023_03_11_114028_create_table_task_time.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTaskTime extends Migration
+class CreateTableTaskTime extends Migration
 {
     /**
      * Run the migrations.
