8c98a62e00cdfa8cdd55ce89ba42845da9615773
OrxataGuy <orxataguy@gmail.com>
2023-03-13 21:23:13 +0100

    pass

--- app/Http/Middleware/SetAppConfigMiddleware.php
+++ app/Http/Middleware/SetAppConfigMiddleware.php
@@ -27,7 +27,7 @@ class SetAppConfigMiddleware
             }
         }else {
             $url = $request->get('url');
-            dd(Request::url());
+            dd($request);
         }
         return $next($request);
     }
