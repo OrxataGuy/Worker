34876e29d4d58512c28b8e90d409299c9babcf9e
OrxataGuy <orxataguy@gmail.com>
2023-03-13 22:00:35 +0100

    pass

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -113,6 +113,8 @@ window.addEventListener("beforeinstallprompt", (e) => {
   e.preventDefault();
   // Stash the event so it can be triggered later.
   deferredPrompt = e;
+
+  console.log(e);
   // Update UI to notify the user they can add to home screen
   addBtn.style.display = "block";
 
