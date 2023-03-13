2bb494b5e3bd37b6a833b1362de2f73fe985c2e0
OrxataGuy <orxataguy@gmail.com>
2023-03-13 22:28:58 +0100

    pass

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -50,7 +50,7 @@ class Handler extends ExceptionHandler
 
     public function render($request, Throwable $exception)
     {
-        dd($exception->getCode());
+        dd($exception);
         return parent::render($request, $exception);
     }
 }
