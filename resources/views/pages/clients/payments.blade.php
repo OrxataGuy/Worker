e27e516ed007e3857d73a5269c12792da728e4b3
OrxataGuy <orxataguy@gmail.com>
2023-03-14 15:28:15 +0100

    pass

--- resources/views/pages/clients/payments.blade.php
+++ resources/views/pages/clients/payments.blade.php
@@ -80,3 +80,40 @@
     <!-- /.card-body -->
   </div>
 @endsection
+
+@section('scripts')
+<script>
+    function confirmForm(id) {
+        Swal.fire({
+            title: '¿Seguro que quieres confirmar el pago?',
+            text: "Una vez el pago sea confirmado, la fecha del pago se actualizará a la actual.",
+            confirmButtonText: 'Confirmar pago'
+        }).then(res => {
+            if(res.isConfirmed) {
+                $.ajax({
+                    type: 'PUT',
+                    url: "{{ route('payments.update', ['payment' => ':id']) }}".replace(':id', id),
+                    success: e => Swal.fire('Pago confirmado correctamente', 'La página se va a recargar.', 'success').then(() => location.reload())
+                })
+            }
+        })
+    }
+
+    function deleteForm(id) {
+        Swal.fire({
+            title: '¿Seguro que quieres eliminar el pago?',
+            text: "Esta acción no se puede deshacer. No quedará ningún registro de este pago.",
+            confirmButtonText: 'Eliminar pago'
+        }).then(res => {
+            if(res.isConfirmed) {
+                $.ajax({
+                    type: 'DELETE',
+                    url: "{{ route('payments.delete', ['payment' => ':id']) }}".replace(':id', id),
+                    data: {id: id},
+                    success: e => Swal.fire('Pago eliminado correctamente', 'La página se va a recargar.', 'success').then(() => location.reload())
+                })
+            }
+        })
+    }
+</script>
+@endsection
