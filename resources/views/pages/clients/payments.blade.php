e184a3a9c91fee0e9c2fed541ce70f5ac52ae13b
OrxataGuy <orxataguy@gmail.com>
2023-03-14 15:27:31 +0100

    pass

--- resources/views/pages/clients/payments.blade.php
+++ resources/views/pages/clients/payments.blade.php
@@ -9,8 +9,7 @@
 @section('clients-section', 'active')
 @section('title', 'Clientes')
 @section('breadcrumb')
-    <li class="breadcrumb-item"><a href="{{ route('clients.index') }}">Clientes</a></li>
-    <li class="breadcrumb-item active">Pagos de {{ $client->name }}</a></li>
+
 @endsection
 @section('content')
 
