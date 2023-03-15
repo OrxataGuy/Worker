895a806e1ff0c0b42f35af51a885d7ab301cc731
OrxataGuy <orxataguy@gmail.com>
2023-03-15 10:59:54 +0100

    noti

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,8 +7,10 @@ use \Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 
+
 class UserController extends Controller
 {
+
     public function passwordChange(Request $request) : JsonResponse
     {
         $user = User::find(auth()->user()->id);
